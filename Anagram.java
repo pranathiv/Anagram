ab15d9ebc54efc87d73bee7b58116d140717c399
Pushpinder Singh <pushpisingh@paypal.com>
2016-08-04 11:53:49 +0530

    Formtted

--- Anagram.java
+++ Anagram.java
@@ -4,66 +4,63 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Anagrams {
-	
+
 	// Main function calling all the other functions
 	public static void main(String[] args) {
-		ArrayList<String> words = getWordsFromFile("C:\\Users\\desriram\\workspace\\Anagrams\\src\\sowpods.txt");
+		ArrayList<String> words = getWordsFromFile("C:\\Users\\pushpisingh\\Desktop\\sowpods.txt");
 		ArrayList<String> wordsCopy = (ArrayList<String>) words.clone();
 		sortAllWords(wordsCopy);
-		printAnagrams(putIntoHashMap(wordsCopy),words);
-		
+		printAnagrams(putIntoHashMap(wordsCopy), words);
 	}
-	
-	// Function to print the anagrams	
-	private static void printAnagrams(HashMap<String, ArrayList<Integer>> hash,ArrayList<String> words) {
-		
-		for(ArrayList<Integer> list : hash.values()) {
-			if(list.size() > 1) {
-				for(Integer l : list)
-					p(words.get(l)+" ");
-				p("\n");
+
+	// Function to print the anagrams
+	private static void printAnagrams(HashMap<String, ArrayList<Integer>> hash, ArrayList<String> words) {
+
+		for (ArrayList<Integer> list : hash.values()) {
+			if (list.size() > 1) {
+				for (Integer l : list)
+					print(words.get(l) + " ");
+				print("\n");
 			}
 		}
 	}
 
 	// Create a hashmap of String --> List of Indices of the anagrams
 	private static HashMap<String, ArrayList<Integer>> putIntoHashMap(ArrayList<String> wordsCopy) {
-		
-		HashMap<String,ArrayList<Integer>> hash = new HashMap<String,ArrayList<Integer>>();
-		for(int i = 0; i < wordsCopy.size(); i++) {
-			if(hash.containsKey(wordsCopy.get(i)) == false) {
+
+		HashMap<String, ArrayList<Integer>> hash = new HashMap<String, ArrayList<Integer>>();
+		for (int i = 0; i < wordsCopy.size(); i++) {
+			if (hash.containsKey(wordsCopy.get(i)) == false) {
 				ArrayList<Integer> al = new ArrayList<Integer>();
 				al.add(i);
 				hash.put(wordsCopy.get(i), al);
 			} else {
 				hash.get(wordsCopy.get(i)).add(i);
 			}
-					
+
 		}
-		p(hash.size()+"\n" );
+		print(hash.size() + "\n");
 		return hash;
-		
+
 	}
 
 	// Sort all the words internally
 	private static void sortAllWords(ArrayList<String> wordsCopy) {
-		
-		for(int i = 0; i < wordsCopy.size(); i++) {
+
+		for (int i = 0; i < wordsCopy.size(); i++) {
 			char[] temp = wordsCopy.get(i).toCharArray();
 			Arrays.sort(temp);
 			wordsCopy.set(i, String.valueOf(temp));
 		}
-			
-		
+
 	}
 
-	private static void p(Object o) {
+	private static void print(Object o) {
 		System.out.print(o);
-		
+
 	}
 
 	// Read all the words from the file
@@ -73,35 +70,33 @@ public class Anagrams {
 		FileInputStream inputStream = null;
 		Scanner sc = null;
 		try {
-		    inputStream = new FileInputStream(path);
-		    sc = new Scanner(inputStream, "UTF-8");
-		    while (sc.hasNextLine()) {
-		        String line = sc.nextLine();
-		        words.add(line);
-		    }
-		    // note that Scanner suppresses exceptions
-		    if (sc.ioException() != null) {
-		        throw sc.ioException();
-		    }
+			inputStream = new FileInputStream(path);
+			sc = new Scanner(inputStream, "UTF-8");
+			while (sc.hasNextLine()) {
+				String line = sc.nextLine();
+				words.add(line);
+			}
+			// note that Scanner suppresses exceptions
+			if (sc.ioException() != null) {
+				throw sc.ioException();
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
-		    if (inputStream != null) {
-		        try {
+			if (inputStream != null) {
+				try {
 					inputStream.close();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-		    }
-		    if (sc != null) {
-		        sc.close();
-		    }
+			}
+			if (sc != null) {
+				sc.close();
+			}
 		}
-		
+
 		return words;
 	}
 }
-
-
